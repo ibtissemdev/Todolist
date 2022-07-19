11a16efad75268bd93861e70f61fd7acb9e1db16
ibtissemdev <ibtissem.khiri@gmail.com>
2022-07-19 11:33:15 +0200

    input entrer une tache ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,35 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfLmi0L9f7y2OEBNzCXNWFzspbI-sK3vU",
+  authDomain: "test-firebase-efed8.firebaseapp.com",
+  projectId: "test-firebase-efed8",
+  storageBucket: "test-firebase-efed8.appspot.com",
+  messagingSenderId: "510078060782",
+  appId: "1:510078060782:web:38b6662fce1eb978140bff"
+};
+
+
+// const app = initializeApp(firebaseConfig);
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+
+
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+
 })
 export class HomePage {
+  currentDate: string;
+  myTask = '';
+  addTask: boolean;
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase) {
 
+    const timeformat: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour12: false
+    };
+
+    this.currentDate = new Date().toLocaleTimeString('fr-FR', timeformat);
+  }
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+  }
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
 }
